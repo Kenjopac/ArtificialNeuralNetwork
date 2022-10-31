0bc9ebdbeebab6ff63a5951b3f5742a3e9837bbf
Kenjopac <83781240+Kenjopac@users.noreply.github.com>
2022-10-30 21:33:48 -0400

    back prop maybe?

--- main.py
+++ main.py
@@ -43,13 +43,35 @@ def forwardprop(input, weights, bias, activationfunc):  # input as 1d array, wei
 def meanSquaredError(predicted, observed):
     return [math.pow(predicted[i] - observed[i] for i in range(len(observed), 2))]
 def AverageCost(predictedlist,Observedlist):
-    return sum(meanSquaredError(predictedlist,Observedlist)) / float(len(predictedlist))
+    return sum(meanSquaredError(predictedlist , Observedlist)) / float(len(predictedlist))
+
+def BackProp(predictedlist, observedlist, w2, xvalues, alpha):
+    m = len(observedlist)
+    dCostdw = [(-2 / m) * sum([observedlist[n] - predictedlist[n] for n in range(m)]) * smval for smval in predictedlist]
+    dCostdw = [dCostdw[i] * xvalues[i] for i in range(len(dCostdw))]
+    w2 = [w2[t] - alpha * dCostdw[t] for t in range(len(dCostdw))]
+    return w2
+
+def PenguinsToDigits(trainset):
+
+    return [0 if n == "Adelie" else 1 if n == "Chinstrap" else 2 for n in trainset]
+
+def oneHotY(observed):
+    a = [[0,0,0 ] for i in range(len(observed))]
+    for x in range(len(observed)):
+        a[x][observed[x]] = 1
+    return a
 
 if __name__ == "__main__":
     penguins = load_penguins()
     x, y = load_penguins(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=100, random_state=0)
     w1, b1, w2, b2 = setup()
-    UnactivatedLayerOne, ActivatedLayerOne = forwardprop(X_train.values.tolist()[0], w1, b1, sigmoid)
-    UnactivatedLayerTwo, ActivatedLayertwo = forwardprop(ActivatedLayerOne, w2, b2, softmax)
-    print(UnactivatedLayerOne, ActivatedLayerOne, UnactivatedLayerTwo, ActivatedLayertwo)
+    firstdataset = X_train.values.tolist()[0]
+    ObservedYvectors = oneHotY(PenguinsToDigits(y_train.tolist()))
+    UnactLayer1, ActLayer1 = forwardprop(firstdataset, w1, b1, sigmoid)
+    UnactLayer2, ActLayer2 = forwardprop(ActLayer1, w2,b2, softmax)
+    for l in w2:
+        print("original weights: ", l)
+        print("updated weights: ", BackProp(ActLayer2, ObservedYvectors[0] , l , UnactLayer2, 0.01))
+
